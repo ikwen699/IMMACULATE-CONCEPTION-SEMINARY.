d03eb91a2f1b3676ca76cab4c9cbaa217c6370e9
Goodluck Ikwen <deochannel08@gmail.com>
2026-09-01 12:22:33 +0100

    nah small bracket wey i miss ooh

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -107,6 +107,7 @@ export async function POST(request: NextRequest) {
           return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
         }
         parentId = currentParentUser?.id
+      }
 
       const { error: sErr } = await supabase
         .from('Student')
